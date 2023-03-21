fade50a62b2c8306c769e8d5e3ee799d26b31d00
nhtung96 <nh.tung@outlook.com>
2023-03-22 04:12:43 +0700

    added transaction history

--- app.py
+++ app.py
@@ -108,25 +108,7 @@ def create_wallet():
 
 @app.route('/history')
 def history():
-    return render_template('history.html')
-
-@app.route('/balance')
-def balance():
-    return render_template('balance.html')
-
-@app.route('/get_balance', methods=['POST'])
-def get_balance():
-    # Get address from query parameter
-    address = request.form['address']
-
-    # Check if address is valid
-    if not w3.isAddress(address):
-        return 'Invalid address!', 400
-
-    # Get the account balance
-    balance = w3.eth.get_balance(address)
-    balance = w3.fromWei(balance, "ether")
-    return render_template('balance.html', address=address, balance=balance)
+    return render_template('history.html', address = main_wallet_api.address, transactions=main_wallet_api.transactions)
 
 @app.route('/transaction')
 def transaction():
@@ -137,14 +119,27 @@ def send_transaction():
     # Get the user input from the HTML form
     recipient_address = request.form['recipient_address']
     amount = request.form['amount']
-    sender_address = request.form['sender_address'] # Replace with your actual private key
+    sender_address = request.form['sender_address'] 
+    private_key = request.form['private_key']
+
 
     # Create the transaction
-    tx_hash = w3.eth.send_transaction({
-    'from': sender_address,
-    'to': recipient_address,
-    'value': w3.toWei(amount, 'ether'),
-})
+    nonce = w3.eth.getTransactionCount(sender_address)
+    txn_dict = {
+        'from': sender_address,
+        'to': recipient_address,
+        'value': w3.toWei(amount, 'ether'),
+        'nonce': nonce,
+        'gas': 200000,
+        'gasPrice': w3.toWei('50', 'gwei'),
+    }
+
+    # Sign transaction
+    signed_txn = w3.eth.account.signTransaction(txn_dict, private_key=private_key)
+
+    # Send transaction
+    tx_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+
     return render_template('transaction.html', recipient_address=recipient_address, amount=amount, hash = tx_hash, \
                                                                                 sender_address=sender_address)
 
