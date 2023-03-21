fef03956c12c721367cdab09c848f2d583210f31
lyngo <ly.ngo@mservice.com.vn>
2023-03-21 21:20:31 +0700

    update wallet functions

--- account/wallet.py
+++ account/wallet.py
@@ -18,42 +18,53 @@ class Transaction():
             <li>Transcation Hash: {}</li>
             <li>From: {}</li>
             <li>To: {}</li>
-            <li>To: value</li>
+            <li>To: {} ETH</li>
         </ul>
-        """.format(self.no, self._hash, self._from, self._to, self._amount)
+        """.format(self.no, self._hash, self._from, self._to, '%.2f' % (self._amount))
         
 # Initialize web3 provider
-class Account():
+class Wallet():
     def __init__(self, _web3: Web3):
         self.wb3 = _web3
         self.address =  ""
+        self.account_name = ""
         self.private_key = ""
         self.balance = 0
         self.transactions = []
 
-    def restore_account(self, private_key):
+    def restore_wallet(self, private_key, account_name):
         account = self.wb3.eth.account.privateKeyToAccount(private_key)
+        # print("After decrypt: ", account.address)
         print(account.address)
         if not self.wb3.isAddress(account.address):
             return 'Invalid address!'
         
         self.address = account.address
         self.private_key = private_key
-        self.update_account()
+        self.account_name = account_name
+        self.update_wallet()
         return "Success"
 
     
-    def register_account(self): 
+    def create_wallet(self): 
         _account = self.wb3.eth.account.create()
         self.address = _account.address
-        self.private_key = _account.key
-        self.update_account()
+        self.private_key = self.wb3.toHex(_account.key)
+        self.update_wallet()
+        print("New address: ", self.address)
         return 'Create Account Successfully!!!'
     
-    def update_account(self):
+    def update_wallet(self):
         self._get_balance()
         self._get_transaction()
 
+    def getPKHex(self):
+        try:
+            pk = self.wb3.toHex(self.private_key)
+            return pk
+        except:
+            return self.private_key
+    
     def _get_balance(self):
         # Get the account balance
         self.balance = self.wb3.eth.get_balance(self.address)
@@ -75,16 +86,30 @@ class Account():
                     if self.address == trans['from'] :
                         if trans['from'] != trans['to']:
                             bal = bal + trans.value
-                            # self.transactions.append(Transaction(j, trans['from'], trans['to'], trans['hash'], trans['value'], trans).to_html)
-                            self.transactions.append("Transaction {}:\n  - Transaction hash: {}\n  - From: {}\n  - To: {}\n  - Amount: {}\n"
-                                                     .format(j, trans['hash'], trans['from'], trans['to'], trans['value']))
+                            transaction = Transaction(j, trans['from'], trans['to'], self.wb3.toHex(trans['hash']), \
+                                                      self.wb3.fromWei(trans['value'], "ether"), trans)
+                            self.transactions.append(transaction)
+                            # self.transactions.append("Transaction {}:\n  - Transaction hash: {}\n  - From: {}\n  - To: {}\n  - Amount: {}\n"
+                            #                          .format(j, trans['hash'], trans['from'], trans['to'], trans['value']))
                             j = j + 1
                     
                     if (self.address == trans.to):
                         if trans['from'] != trans['to']:
                             bal = bal - trans['value']
-                            # self.transactions.append(Transaction(j, trans['from'], trans['to'], trans['hash'], trans['value'], trans).to_html())
-                            self.transactions.append("Transaction {}:\n  - Transaction hash: {}\n  - From: {}\n  - To: {}\n  - Amount: {}\n"
-                                                     .format(j, trans['hash'], trans['from'], trans['to'], trans['value']))
+                            transaction = Transaction(j, trans['from'], trans['to'], self.wb3.toHex(trans['hash']), \
+                                                      self.wb3.fromWei(trans['value'], "ether"), trans)
+                            self.transactions.append(transaction)
+                            # self.transactions.append("Transaction {}:\n  - Transaction hash: {}\n  - From: {}\n  - To: {}\n  - Amount: {}\n"
+                            #                          .format(j, trans['hash'], trans['from'], trans['to'], trans['value']))
                             j = j + 1
             i = i - 1
+
+    def get_keystore(self, password):
+        return self.wb3.eth.account.encrypt(self.private_key, password)
+
+    def restore_by_keystore(self, keystore, password, account_name):
+        private_key = self.wb3.eth.account.decrypt(keystore, password)
+        return self.restore_wallet(private_key, account_name=account_name)
+
+    def update_account_name(self, account_name):
+        self.account_name = account_name
--- app.py
+++ app.py
@@ -1,51 +1,110 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from web3 import Web3, HTTPProvider
 # from eth_account import Account
-from accounts import account
+from account import wallet
+from user import user_model as dbHandler
+from account import wallet_model as dbWallet
+import json
 
 app = Flask(__name__)
 
 # Initialize web3 provider
 w3 = Web3(HTTPProvider('http://localhost:7545'))
 ## Global account
-main_account = account.Account(w3)   
-
-
-@app.route('/')
-def index():
-    return render_template('index.html', account_address = main_account.address)
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
+main_wallet_api = wallet.Wallet(w3)   
+main_account = dbHandler.UserAccount()
+list_wallet = dbWallet.ListWalletsModel()
 
 @app.route('/account')
 def account():
-    main_account.update_account()
-    return render_template('account.html', address = main_account.address, \
-                            balance = main_account.balance, transactions=main_account.transactions)
+    print("main_account.username", main_account.username)
+    list_wallet.retrieveWallet(main_account.username)
+    return render_template('account.html', account_address = main_wallet_api.address, \
+                           username=main_account.username, list_wallet = list_wallet.listWallet)
 
-@app.route('/login')
-def login():
-	return render_template('login.html')
-
-@app.route('/restore_account', methods=['POST'])
-def restore():
-    private_key = request.form['private_key']
-    result = main_account.restore_account(private_key)
-
-    if result == "Success":
-        # return render_template('register.html', address = main_account.address, private_key = main_account.private_key)
-        return render_template('account.html', address = main_account.address, \
-                            balance = main_account.balance, transactions=main_account.transactions)
+@app.route('/about')
+def about():
+    print(main_account.username)
+    if main_account.username != "":
+        return render_template('about.html', username = main_account.username)
     else:
-        return render_template('login.html', error=result)
+        return render_template('about.html')
 
-@app.route('/register')
-def register():
+@app.route('/backup', methods=['POST'])
+def backup():
+    password = request.form["password"]
+    print(password, main_account.password)
+    if password == main_account.password:
+        return render_template('backup.html', private_key=main_wallet_api.getPKHex())
+    else:
+        return render_template('backup.html', msg="Wrong passphrase")
+
+@app.route('/restore_by_keystore', methods=['POST'])
+def restore_by_keystore():
+    account_name = request.form['account_name']
+    # print("account_name", account_name)
+    _wallet = list_wallet.select_account(account_name=account_name)
+    # print(account_name, _wallet.account_name, _wallet.address)
+    result = main_wallet_api.restore_by_keystore(json.loads(_wallet.keystore),\
+                                                 main_account.password, account_name=account_name) 
+    # print(main_wallet_api.address)
+    if request.form['action'] == 'Choose':
+        ## Restore wallet account 
+        if result == "Success":
+            list_wallet.insertWallet(main_account.username, main_wallet_api.address, _wallet.keystore, main_wallet_api.account_name)
+            return redirect("/wallet_info")
+        else:
+            list_wallet.removeWallet(account_name=account_name)
+            return redirect("/account")
+    elif request.form['action'] == 'Remove':
+        list_wallet.removeWallet(account_name=account_name)
+        return redirect("/account")
+    elif request.form['action'] == 'Show Key':
+        return render_template('backup.html')
+
+@app.route('/wallet_info')
+def wallet_info():
+    main_wallet_api.update_wallet()
+    return render_template('wallet.html', address = main_wallet_api.address, \
+                            balance = main_wallet_api.balance, transactions=main_wallet_api.transactions)
+
+@app.route('/restore')
+def restore():
+	return render_template('restore.html')
+
+@app.route('/restore_wallet', methods=['POST'])
+def restore_wallet():
+    private_key = request.form['private_key']
+    account_name = request.form['account_name']
+    result = main_wallet_api.restore_wallet(private_key, account_name)
+    keystore = main_wallet_api.get_keystore(main_account.password)
+    if result == "Success":
+        status, msg = list_wallet.insertWallet(main_account.username, main_wallet_api.address, keystore, main_wallet_api.account_name)
+        if status:
+            return redirect("/wallet_info")
+        else:
+            return render_template('restore.html', error=msg)
+    else:
+        return render_template('restore.html', error=result)
+
+@app.route('/create')
+def create():
     # Register account
-    main_account.register_account()
-    return render_template('register.html', address = main_account.address, private_key = main_account.private_key)
+    main_wallet_api.create_wallet()
+    return render_template('create.html', address = main_wallet_api.address, private_key = main_wallet_api.private_key)
+
+@app.route('/create_wallet', methods=['POST'])
+def create_wallet():
+    # Register account
+    account_name = request.form["account_name"]
+    main_wallet_api.update_account_name(account_name=account_name)
+    keystore = main_wallet_api.get_keystore(main_account.password)
+    status, msg = list_wallet.insertWallet(main_account.username, main_wallet_api.address, keystore, account_name)
+    print("Save DB: ",  main_wallet_api.address)
+    if status:
+        return redirect("/wallet_info")
+    else:
+        return render_template('create.html', address = main_wallet_api.address, private_key = main_wallet_api.private_key, msg=msg)
 
 @app.route('/history')
 def history():
@@ -71,7 +130,7 @@ def get_balance():
 
 @app.route('/transaction')
 def transaction():
-    return render_template('transaction.html', sender_address = main_account.address)
+    return render_template('transaction.html', sender_address = main_wallet_api.address)
 
 @app.route('/send_transaction', methods=['POST'])
 def send_transaction():
@@ -88,6 +147,38 @@ def send_transaction():
 })
     return render_template('transaction.html', recipient_address=recipient_address, amount=amount, hash = tx_hash, \
                                                                                 sender_address=sender_address)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method=='POST':
+        username = request.form['username']
+        password = request.form['password']
+        status, msg = main_account.retrieveUsers(username=username, password=password)
+        if status:
+            return redirect("/account") 
+        else:
+            return render_template('login.html', msg=msg)
+    else:
+        return render_template('login.html')
+    
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method=='POST':
+        username = request.form['username']
+        password = request.form['password']
+        status, msg = main_account.insertUser(username=username, password=password)
+        if status:
+            return redirect("/account") 
+        else:
+            return render_template('register.html', msg=msg)
+    else:
+        return render_template('register.html')
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    main_account.logout()
+    return render_template("index.html")
 
 if __name__ == '__main__':
 	app.run(debug=True)
